47ba4662c5680f3034985791a9e9b802d0bc97a4
Jeremy Persing <34422870+JeremyPersing@users.noreply.github.com>
2021-12-16 16:35:41 -0800

    initial commit

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,21 +1,19 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'JeremyPersing.com',
+  author: 'Jeremy Persing',
+  headerTitle: '',
+  description:
+    'The portfolio and blog of Jeremy Persing. Here I will show my work and document how I created projects, along with any problems that I ran into and how I solved them.',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
-  siteLogo: '/static/images/logo.png',
-  image: '/static/images/avatar.png',
+  siteUrl: 'https://jeremypersing.com',
+  siteLogo: '/static/images/jeremypersinglogo.svg',
+  image: '/static/images/portrait.jpg',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  email: 'jpersing1999@gmail.com',
+  github: 'https://github.com/JeremyPersing',
+  youtube: 'https://www.youtube.com/channel/UCtLAhqH5Mezxt7DY9pcnOyg',
+  linkedin: 'https://www.linkedin.com/in/jeremy-persing/',
   locale: 'en-US',
   analytics: {
     // supports plausible, simpleAnalytics or googleAnalytics
--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -3,7 +3,7 @@ import Image from '@/components/Image'
 import { PageSEO } from '@/components/SEO'
 
 export default function AuthorLayout({ children, frontMatter }) {
-  const { name, avatar, occupation, company, email, twitter, linkedin, github } = frontMatter
+  const { name, avatar, occupation, company, email, youtube, linkedin, github } = frontMatter
 
   return (
     <>
@@ -11,7 +11,7 @@ export default function AuthorLayout({ children, frontMatter }) {
       <div className="divide-y">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            About
+            About Me
           </h1>
         </div>
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
@@ -30,7 +30,7 @@ export default function AuthorLayout({ children, frontMatter }) {
               <SocialIcon kind="mail" href={`mailto:${email}`} />
               <SocialIcon kind="github" href={github} />
               <SocialIcon kind="linkedin" href={linkedin} />
-              <SocialIcon kind="twitter" href={twitter} />
+              <SocialIcon kind="youtube" href={youtube} />
             </div>
           </div>
           <div className="pt-8 pb-8 prose dark:prose-dark max-w-none xl:col-span-2">{children}</div>
